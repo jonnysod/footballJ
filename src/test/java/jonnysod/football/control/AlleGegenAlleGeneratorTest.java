d995214c8a2576262bc109a7377c7eccefd9a19c
Jonathan Hellwig <j.hellwig@sonnen.de>
2020-04-14 22:49:04 +0200

    Add test that number of spiele of each team is the same.

--- src/test/java/jonnysod/football/control/AlleGegenAlleGeneratorTest.java
+++ src/test/java/jonnysod/football/control/AlleGegenAlleGeneratorTest.java
@@ -3,15 +3,20 @@
  */
 package jonnysod.football.control;
 
+import jonnysod.football.model.Spiel;
 import jonnysod.football.model.Spieler;
 import jonnysod.football.model.Spieltag;
 import jonnysod.football.model.Team;
+import jonnysod.football.statistic.SpielInfo;
 import net.jqwik.api.constraints.Size;
 import net.jqwik.api.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.google.common.collect.Iterables.size;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 public class AlleGegenAlleGeneratorTest {
@@ -24,10 +29,39 @@ public class AlleGegenAlleGeneratorTest {
         assertTrue(spieltag.size() >= teamList.size());
     }
 
+    @Property
+    public void testAllSameNumberOfSpiele(@ForAll @Size(min = 2, max = 10)List<@From("genTeam") Team> teamList) {
+        SpieltagGenerator generator = new AlleGegenAlleGenerator();
+        TurnierOptionen optionen = new TurnierOptionen(60);
+        Spieltag spieltag = generator.generateSpieltag(teamList, optionen);
+
+        Map<Team, Integer> numberOfSpieleMap = new HashMap<>();
+        for (Team team: teamList) {
+            int numberOfSpiele = 0;
+            if (numberOfSpieleMap.get(team) != null) {
+                numberOfSpiele = numberOfSpieleMap.get(team);
+            }
+            for (Spiel spiel: spieltag ) {
+                if(new SpielInfo(spiel).teilnehmer(team)) {
+                    numberOfSpiele++;
+                }
+            }
+            numberOfSpieleMap.put(team, numberOfSpiele);
+        }
+        Integer numberOfSpiele = null;
+        for(Integer numberOfSpielTeam: numberOfSpieleMap.values()) {
+            if (numberOfSpiele == null) {
+                numberOfSpiele = numberOfSpielTeam;
+            } else {
+                assertSame(numberOfSpiele, numberOfSpielTeam);
+            }
+        }
+    }
+
     @Provide
     Arbitrary<Team> genTeam() {
         Arbitrary<String> nameGen = Arbitraries.strings().withCharRange('a', 'z')
-                .ofMinLength(3).ofMaxLength(10);
+                .ofMinLength(3).ofMaxLength(10).unique();
         Arbitrary<Spieler> spielerGen = genSpieler();
         Arbitrary<List<Spieler>> spielerListGen = spielerGen.collect(list -> size(list) > 5);
         return Combinators.combine(nameGen, spielerListGen).as((name, spielerList) -> {
